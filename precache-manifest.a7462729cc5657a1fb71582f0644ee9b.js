fb05502b0f0104543cf7a209642d0b8c859c3d1f
Andrison1425 <71196781+Andrison1425@users.noreply.github.com>
2020-12-16 19:17:13 -0500

    Add files via upload

---
+++ precache-manifest.a7462729cc5657a1fb71582f0644ee9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad333f4f717006ded6c9a215bd233a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd524a8578c1fcc70265",
+    "url": "/static/css/main.ce6764e0.chunk.css"
+  },
+  {
+    "revision": "64ea64e4454039a89bca",
+    "url": "/static/js/2.ca3e3134.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ca3e3134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd524a8578c1fcc70265",
+    "url": "/static/js/main.a5a8c92d.chunk.js"
+  },
+  {
+    "revision": "29630ad1ca7e967d236a",
+    "url": "/static/js/runtime-main.ea644902.js"
+  }
+]);
